4a85deb29d7f398679b63be7fa975dd6a71a1b82
mikepierredotcom@gmail.com <mikepierredotcom@gmail.com>
2019-10-23 15:46:01 -0400

    Updates & Additions
    Refectored prexisitng classes, and added new functionality.

--- src/main/java/sdk/Controller/Hotels.java
+++ src/main/java/sdk/Controller/Hotels.java
@@ -1,6 +1,32 @@
 package sdk.Controller;
 
-public class Hotels 
-{
-    
+import java.io.IOException;
+import java.util.HashMap;
+
+import sdk.Helpers.CurlRequest;
+
+public class Hotels {
+    public String book(HashMap<String, String> data) {
+        String response;
+        CurlRequest curlRequest = new CurlRequest();
+        try {
+            response = curlRequest.sendRequest(data);
+            return response;
+        } catch (IOException e) {
+             e.printStackTrace();
+        }
+        
+
+        return null;
+    }
+    public String getExternalMarkets()
+    {
+        //
+    }
+    public String getIds(){}
+    public String getImages(){}
+    public String getProfileId(){}
+    public String getRates(){}
+    public String searchHotels(){}
+
 }
---
+++ src/main/java/sdk/POJO/Config.java
@@ -0,0 +1,159 @@
+package sdk.POJO;
+
+public class Config {
+    
+    private String auth;
+    private String hotelsBook;
+    private String hotelsGetExternalMarkets;
+    private String hotelsGetIds;
+    private String hotelsGetImages;
+    private String hotelsGetProfileById;
+    private String hotelsGetLocations;
+    private String hotelsGetRates;
+    private String hotelsSearch;
+    private String carsBook;
+    private String carsCancel;
+    private String carLocations;
+    private String carRates;
+    private String attractionTicketsBook;
+    private String attractionTicketsGetRates;
+    private String transportSearch;
+    private String transportBook;
+
+    public String getAuth() {
+        return auth;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getHotelsBook() {
+        return hotelsBook;
+    }
+
+    public void setHotelsBook(String hotelsBook) {
+        this.hotelsBook = hotelsBook;
+    }
+
+    public String getHotelsGetExternalMarkets() {
+        return hotelsGetExternalMarkets;
+    }
+
+    public void setHotelsGetExternalMarkets(String hotelsGetExternalMarkets) {
+        this.hotelsGetExternalMarkets = hotelsGetExternalMarkets;
+    }
+
+    public String getHotelsGetIds() {
+        return hotelsGetIds;
+    }
+
+    public void setHotelsGetIds(String hotelsGetIds) {
+        this.hotelsGetIds = hotelsGetIds;
+    }
+
+    public String getHotelsGetImages() {
+        return hotelsGetImages;
+    }
+
+    public void setHotelsGetImages(String hotelsGetImages) {
+        this.hotelsGetImages = hotelsGetImages;
+    }
+
+    public String getHotelsGetProfileById() {
+        return hotelsGetProfileById;
+    }
+
+    public void setHotelsGetProfileById(String hotelsGetProfileById) {
+        this.hotelsGetProfileById = hotelsGetProfileById;
+    }
+
+    public String getHotelsGetLocations() {
+        return hotelsGetLocations;
+    }
+
+    public void setHotelsGetLocations(String hotelsGetLocations) {
+        this.hotelsGetLocations = hotelsGetLocations;
+    }
+
+    public String getHotelsGetRates() {
+        return hotelsGetRates;
+    }
+
+    public void setHotelsGetRates(String hotelsGetRates) {
+        this.hotelsGetRates = hotelsGetRates;
+    }
+
+    public String getHotelsSearch() {
+        return hotelsSearch;
+    }
+
+    public void setHotelsSearch(String hotelsSearch) {
+        this.hotelsSearch = hotelsSearch;
+    }
+
+    public String getCarsBook() {
+        return carsBook;
+    }
+
+    public void setCarsBook(String carsBook) {
+        this.carsBook = carsBook;
+    }
+
+    public String getCarsCancel() {
+        return carsCancel;
+    }
+
+    public void setCarsCancel(String carsCancel) {
+        this.carsCancel = carsCancel;
+    }
+
+    public String getCarLocations() {
+        return carLocations;
+    }
+
+    public void setCarLocations(String carLocations) {
+        this.carLocations = carLocations;
+    }
+
+    public String getCarRates() {
+        return carRates;
+    }
+
+    public void setCarRates(String carRates) {
+        this.carRates = carRates;
+    }
+
+    public String getAttractionTicketsBook() {
+        return attractionTicketsBook;
+    }
+
+    public void setAttractionTicketsBook(String attractionTicketsBook) {
+        this.attractionTicketsBook = attractionTicketsBook;
+    }
+
+    public String getAttractionTicketsGetRates() {
+        return attractionTicketsGetRates;
+    }
+
+    public void setAttractionTicketsGetRates(String attractionTicketsGetRates) {
+        this.attractionTicketsGetRates = attractionTicketsGetRates;
+    }
+
+    public String getTransportSearch() {
+        return transportSearch;
+    }
+
+    public void setTransportSearch(String transportSearch) {
+        this.transportSearch = transportSearch;
+    }
+
+    public String getTransportBook() {
+        return transportBook;
+    }
+
+    public void setTransportBook(String transportBook) {
+        this.transportBook = transportBook;
+    }
+    
+}
